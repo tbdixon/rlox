87466af5b852f0a8693c90f2d95c968c9ff06c7b
Blake Dixon <tbdixon@gmail.com>
2020-09-24 20:39:43 -0400

    Compiles! Handles single key and val

--- rust-misc/hashmap/src/main.rs
+++ rust-misc/hashmap/src/main.rs
@@ -1,54 +1,64 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::mem;
 
 type Node<'a> = Option<Box<Link<'a>>>;
 
 struct Link<'a> {
     key: &'a str,
     val: i32,
-    next: Node<'a>
+    next: Node<'a> // None or Box<Link>
 }
 
 const INIT_BUCKET_COUNT: usize = 1024;
 pub struct HashTable<'a> {
-    numElems: i32,
-    numBuckets: usize,
+    num_elems: i32,
+    num_buckets: usize,
     buckets: Vec<Node<'a>>
 }   
 
 impl<'a> Link<'a>{
     pub fn new(key: &'a str, val: i32) -> Self {
         Link{ key, val, next : None }
-    }   
+    }
 }
 
-impl HashTable<'_>{
+impl<'a> HashTable<'a>{
     pub fn new() -> Self {
-        HashTable{ numElems : 0, numBuckets: INIT_BUCKET_COUNT, buckets : Vec::with_capacity(INIT_BUCKET_COUNT) }
+        let mut buckets = Vec::with_capacity(INIT_BUCKET_COUNT);
+        for _ in 0..INIT_BUCKET_COUNT {
+            buckets.push(None);
+        }
+        HashTable{ num_elems : 0, num_buckets: INIT_BUCKET_COUNT, buckets }
     }
 
-    pub fn put(&self, key: &str, val: i32) {
+    pub fn put(&mut self, key: &'a str, val: i32) {
         // This looks... terrible. 
-        let hash_val = ( get_hash(key) % (self.numBuckets as u64) ) as usize;
-        println!("{}", hash_val);
-        let link = match self.buckets.get(hash_val) {
-            None => {
-                let new_link = Link::new(key,val);
-                Box::new(new_link)
+        let hash_val = ( get_hash(key) % (self.num_buckets as u64) ) as usize;
+        let mut new_link = Link::new(key,val);
+        if let Some(n) = self.buckets.get_mut(hash_val) {
+            //N is &mut Node
+            if let Some(l) = mem::replace(n, None) {
+                //L is Box<Link>
+                new_link.next = Some(l);
             }
-            Some(L) => {
-                // Loop through and see if exists
-                *L
-            }
-        };
-        link.next = self.buckets.get(hash_val);
-        self.buckets[hash_val] = link;
+        }
+        self.buckets[hash_val] = Some(Box::new(new_link));
+        self.num_elems += 1;
     }
 
-    pub fn get(&self, key: &str) -> i32 {
-        let hash_val = get_hash(key) % (self.numBuckets as u64);
-        return 5;
-    }   
+    pub fn get(&self, key: &str) -> Option<i32> {
+        let hash_val = ( get_hash(key) % (self.num_buckets as u64) ) as usize;
+        match self.buckets.get(hash_val) {
+            None => None,
+            Some(n) => {
+                match n {
+                    None => None,
+                    Some(l) => Some(l.val)
+                }  
+            }
+        }
+    }
 }
 
 fn get_hash(key: &str) -> u64 {
@@ -58,8 +68,9 @@ fn get_hash(key: &str) -> u64 {
 }
 
 fn main() {
-    let my_map = HashTable::new();
-    my_map.put(&"K1", 1)
+    let mut my_map = HashTable::new();
+    my_map.put(&"K1", 1);
+    println!("{:?}", my_map.get(&"K1"))
 }
 
 #[cfg(test)]
@@ -69,7 +80,14 @@ mod tests {
 
     #[test]
     fn hash_fn() {
-        assert_eq!(7982898449168957443, get_hash("Hello World".to_string()))
+        assert_eq!(7982898449168957443, get_hash(&"Hello World"))
+    }
+    
+    #[test]
+    fn single_value() {
+        let mut my_map = HashTable::new();
+        my_map.put(&"K1", 1);
+        assert_eq!(Some(1), my_map.get(&"K1")) 
     }
 
     #[test]
