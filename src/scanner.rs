014b1471a35f70d97b182377ba3fe5230088328f
Blake Dixon <tbdixon@gmail.com>
2020-12-02 19:05:38 -0500

    refactoring, adding actual scanning

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,7 +2,10 @@ use itertools::multipeek;
 use itertools::structs::MultiPeek;
 use std::iter::Enumerate;
 use std::str::Chars;
+
+type SourceEnumItem = (usize,char);
 const EOF_CHAR: char = '\0';
+const EOF_MARKER: SourceEnumItem = (usize::MAX, EOF_CHAR);
 
 pub struct Scanner<'a> {
     source: &'a str,
@@ -11,14 +14,29 @@ pub struct Scanner<'a> {
     line_num: i32,
 }
 
-trait MultiPeekExt {
-    fn peek_nth(&mut self, n: usize) -> Option<(usize, char)>;
-    fn peek_next(&mut self) -> Option<(usize, char)>;
-    fn peek_two(&mut self) -> Option<(usize, char)>;
+trait TupleExt {
+    fn ch(&self) -> char;
+    fn idx(&self) -> usize;
 }
 
-impl<I: Iterator<Item = (usize, char)>> MultiPeekExt for MultiPeek<I> {
-    fn peek_nth(&mut self, n: usize) -> Option<(usize, char)> {
+impl TupleExt for SourceEnumItem {
+    fn ch(&self) -> char {
+        self.1
+    }
+
+    fn idx(&self) -> usize {
+        self.0
+    }
+}
+
+trait MultiPeekExt {
+    fn peek_nth(&mut self, n: usize) -> Option<SourceEnumItem>;
+    fn peek_next(&mut self) -> Option<SourceEnumItem>;
+    fn peek_two(&mut self) -> Option<SourceEnumItem>;
+}
+
+impl<I: Iterator<Item = SourceEnumItem>> MultiPeekExt for MultiPeek<I> {
+    fn peek_nth(&mut self, n: usize) -> Option<SourceEnumItem> {
         let mut nth = self.peek();
         for _ in 0..n {
             nth = self.peek();
@@ -30,13 +48,13 @@ impl<I: Iterator<Item = (usize, char)>> MultiPeekExt for MultiPeek<I> {
 
     // Returns the value *after* the one pointed
     // at by Iterator but does not consume
-    fn peek_two(&mut self) -> Option<(usize, char)> {
+    fn peek_two(&mut self) -> Option<SourceEnumItem> {
         self.peek_nth(1)
     }
 
     // Returns the value pointed at by Iterator but
     // does not consume
-    fn peek_next(&mut self) -> Option<(usize, char)> {
+    fn peek_next(&mut self) -> Option<SourceEnumItem> {
         self.peek_nth(0)
     }
 }
@@ -93,7 +111,7 @@ pub enum TokenType {
     TOKEN_EOF_CHAR,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Token<'a> {
     pub kind: TokenType,
     pub lexeme: &'a str,
@@ -102,10 +120,10 @@ pub struct Token<'a> {
 
 use crate::scanner::TokenType::*;
 impl Token<'_> {
-    pub fn error(line_num: i32, err_msg: &str) -> Token {
+    pub fn error(line_num: i32, lexeme: &str) -> Token {
         Token {
             kind: TOKEN_ERROR,
-            lexeme: err_msg,
+            lexeme,
             line_num,
         }
     }
@@ -123,6 +141,11 @@ impl Token<'_> {
     }
 }
 
+// Scanner works by iterating over the source file as a Character iterator.
+// The iterator points to the character that will be scanned *next* hence peek_char
+// returns the next up character as well. While scanning this keeps track of the 
+// start of a lexeme and loops to the end of that lexeme, storing that as a 
+// string slice in a token returned to the compiler.
 impl Scanner<'_> {
     pub fn new(source: &str) -> Scanner {
         let source_itr = multipeek(source.chars().enumerate());
@@ -134,11 +157,15 @@ impl Scanner<'_> {
         }
     }
 
-    fn advance(&mut self) -> char {
-        match self.source_itr.next() {
-            Some(c) => c.1,
-            None => EOF_CHAR,
-        }
+    // Moved the iterator to point to the next element and returns the 
+    // current element that is pointed to by the iterator. If the 
+    // iterator is at the end of the source then return the EOF markers.
+    fn advance(&mut self) -> SourceEnumItem {
+        self.source_itr.next().unwrap_or(EOF_MARKER)
+    }
+
+    fn at_end(&mut self) -> bool {
+        self.peek_char() == EOF_CHAR
     }
 
     // Churns through whitespace and comments until next valid character
@@ -152,14 +179,14 @@ impl Scanner<'_> {
     // Starting at x, this will move the iterator to 'f' on the next line.
     fn eat_whitespace_and_comments(&mut self) {
         while self.peek_char().is_whitespace() {
-            if self.advance() == '\n' {
+            if self.advance().ch() == '\n' {
                 self.line_num += 1;
             }
             if self.peek_char() == '/' && self.peek_two_chars() == '/' {
                 self.source_itr
-                    .find(|c| c.1 == '\n')
-                    .unwrap_or((usize::MAX, EOF_CHAR))
-                    .1;
+                    .find(|c| c.ch() == '\n')
+                    .unwrap_or(EOF_MARKER)
+                    .ch();
                 self.line_num += 1;
             }
         }
@@ -167,40 +194,53 @@ impl Scanner<'_> {
 
     // A set of helper functions that handle unwrapping the Options that
     // come back from peek functionality.
-    fn peek_two_chars(&mut self) -> char {
-        match self.source_itr.peek_two() {
-            Some(c) => c.1,
-            None => EOF_CHAR,
-        }
+    fn peek_char(&mut self) -> char {
+        self.source_itr.peek_next().unwrap_or(EOF_MARKER).ch()
     }
 
-    fn peek_char(&mut self) -> char {
-        match self.source_itr.peek_next() {
-            Some(c) => c.1,
-            None => EOF_CHAR,
-        }
+    fn peek_two_chars(&mut self) -> char {
+        self.source_itr.peek_two().unwrap_or(EOF_MARKER).ch()
     }
 
     // Returns usize::MAX for index if at the end of the iterator.
     // Presumably using usize::MAX is a safe sentinal value since
     // source code probably isn't 2^64 - 1 characters long.
     fn peek_idx(&mut self) -> usize {
-        match self.source_itr.peek_next() {
-            Some(c) => c.0,
-            None => usize::MAX,
-        }
+        self.source_itr.peek_next().unwrap_or(EOF_MARKER).idx()
     }
 
     fn current_lexeme(&mut self) -> &str {
-        &self.source[self.start_idx..self.peek_idx()]
+        // If we're at the end, we lose the iterator index so take
+        // the min of "infinity" and the actual length of the original
+        // source string.
+        let end = std::cmp::min(self.source.len(), self.peek_idx());
+        &self.source[self.start_idx..end]
+    }
+
+    // Includes the quotation marks in the lexeme
+    fn string(&mut self) -> Token {
+        while self.peek_char() != '"' {
+            if self.at_end() {
+                return Token::error(self.line_num, "Unterminated string.");
+            }
+            if self.advance().ch() == '\n' {
+                self.line_num += 1;
+            }
+        }
+        self.advance();
+        let line_num = self.line_num;
+        let lexeme = self.current_lexeme();
+        return Token{ kind: TOKEN_STRING, lexeme, line_num }
     }
 
     pub fn scan_token(&mut self) -> Token {
         self.eat_whitespace_and_comments();
-        let c = self.advance();
-        match c {
+        let (start_idx, ch) = self.advance();
+        self.start_idx = start_idx;
+        match ch {
+            '"' => self.string(),
             EOF_CHAR => Token::eof(),
-            _ => Token::error(self.line_num, "Invalid character"),
+            _ => Token::error(self.line_num, self.current_lexeme()),
         }
     }
 }
@@ -210,15 +250,23 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_string() {
+        let source = "\"Hello\"";
+        let mut scanner = Scanner::new(source);
+        let token = scanner.scan_token();
+        assert_eq!(token, Token{ kind: TOKEN_STRING, lexeme: &source[..], line_num: 1 });
+    }
+
+    #[test]
     fn test_advance() {
         let source = "Hello World";
         let mut scanner = Scanner::new(source);
         let mut c: char;
         for (_, val) in source.chars().enumerate() {
-            c = scanner.advance();
+            c = scanner.advance().ch();
             assert_eq!(c, val);
         }
-        c = scanner.advance();
+        c = scanner.advance().ch();
         assert_eq!(c, EOF_CHAR);
     }
 
@@ -231,7 +279,7 @@ mod tests {
         scanner.advance();
         scanner.eat_whitespace_and_comments();
         assert_eq!(scanner.peek_char(), 'H');
-        assert_eq!(scanner.advance(), 'H');
+        assert_eq!(scanner.advance().ch(), 'H');
         assert_eq!(scanner.line_num, 2);
     }
 
